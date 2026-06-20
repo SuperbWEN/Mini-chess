2e6d8f87ceaa42e40cfbc4ab2f98d6905da03731
wen <wentvtv@gmail.com>
2026-06-20 14:34:22 +0800

    add PVS

---
+++ src/policy/PVS.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "state.hpp"
+#include "search_types.hpp"
+#include "game_history.hpp"
+#include "minimax.hpp"
+
+class PVS{
+public:
+    static SearchResult search(
+        State *state,
+        int depth,
+        GameHistory& history,
+        SearchContext& ctx
+    );
+
+    static ParamMap default_params();
+    static std::vector<ParamDef> param_defs();
+};
